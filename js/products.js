91c3444ef90b99c91a528159cb451e6495309a45
YousefEl-Black <99845367+YousefEl-Black@users.noreply.github.com>
2022-02-17 18:30:42 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
 const myProducts = {
   "0001" : {
-    link : "SSSS",
-    name : "SDD",
+    link : "https://beta.picksell.io/product/%d8%a8%d9%88%d8%aa-%d8%ac%d9%84%d8%af-%d8%a8%d8%b1%d8%a8%d8%a7%d8%b7-%d8%a8%d9%8a%d8%ac-%d9%85%d9%82%d8%a7%d8%b338/",
+    name : "بوت جلد برباط بيج",
   }
 }

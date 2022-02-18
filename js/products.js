52f0fed268619dd3dfd273740814bfc2fa1224f2
YousefEl-Black <99845367+YousefEl-Black@users.noreply.github.com>
2022-02-19 01:44:43 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -10,5 +10,9 @@ const myProducts = {
   "0003" : {
     link : "https://beta.picksell.io/product/%d9%85%d9%83%d9%88%d8%a9-%d8%a7%d9%84%d8%b4%d8%b9%d8%b1-%d8%a7%d9%86%d9%81%d8%b1%d8%a7-%d8%b1%d8%af/",
     name : "مكوة الشعر انفرا رد",
+  },
+  "0004" : {
+    link : "https://taager.com/products/61ed9358ccb1560014a91cb0?productName=%D9%81%D8%B1%D8%B4%D8%A7%D8%A9%20%D8%A7%D9%84%D8%B4%D8%B9%D8%B1%20%20SilverCrest%20Creamic",
+    name : "فرشاة الشعر SilverCrest Creamic",
   }
 }

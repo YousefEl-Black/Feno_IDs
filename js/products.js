0dbb418688ca3f1f6f44adcf72f5c44dc84808b9
YousefEl-Black <99845367+YousefEl-Black@users.noreply.github.com>
2022-02-18 22:50:45 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,5 +2,9 @@ const myProducts = {
   "0001" : {
     link : "https://beta.picksell.io/product/%d8%a8%d9%88%d8%aa-%d8%ac%d9%84%d8%af-%d8%a8%d8%b1%d8%a8%d8%a7%d8%b7-%d8%a8%d9%8a%d8%ac-%d9%85%d9%82%d8%a7%d8%b338/",
     name : "بوت جلد برباط بيج",
+  },
+  "0002" : {
+    link : "https://beta.picksell.io/product/%d8%af%d9%81%d8%a7%d9%8a%d8%a9-%d9%83%d9%87%d8%b1%d8%a8%d8%a7%d8%a6%d9%8a%d8%a9-%d8%a8%d9%82%d9%88%d8%a9-2000-%d9%88%d8%a7%d8%b7/",
+    name : "دفاية كهربائية بقوة 2000 واط",
   }
 }

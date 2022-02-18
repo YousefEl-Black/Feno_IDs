84808a33a44ef43a808102bddafd3b79111eb849
YousefEl-Black <99845367+YousefEl-Black@users.noreply.github.com>
2022-02-19 00:54:22 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,5 +6,9 @@ const myProducts = {
   "0002" : {
     link : "https://beta.picksell.io/product/%d8%af%d9%81%d8%a7%d9%8a%d8%a9-%d9%83%d9%87%d8%b1%d8%a8%d8%a7%d8%a6%d9%8a%d8%a9-%d8%a8%d9%82%d9%88%d8%a9-2000-%d9%88%d8%a7%d8%b7/",
     name : "دفاية كهربائية بقوة 2000 واط",
+  },
+  "0003" : {
+    link : "https://beta.picksell.io/product/%d9%85%d9%83%d9%88%d8%a9-%d8%a7%d9%84%d8%b4%d8%b9%d8%b1-%d8%a7%d9%86%d9%81%d8%b1%d8%a7-%d8%b1%d8%af/",
+    name : "مكوة الشعر انفرا رد",
   }
 }

74c88454a41579f1c8f291ebafe68feee968875e
YousefEl-Black <99845367+YousefEl-Black@users.noreply.github.com>
2022-02-19 23:05:37 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -18,5 +18,35 @@ const myProducts = {
     link : "https://taager.com/products/61ed9358ccb1560014a91cb0?productName=%D9%81%D8%B1%D8%B4%D8%A7%D8%A9%20%D8%A7%D9%84%D8%B4%D8%B9%D8%B1%20%20SilverCrest%20Creamic",
     name : "فرشاة الشعر SilverCrest Creamic",
     price : "240 EGP",
-  }
+  },
+  "0005" : {
+    link : "https://taager.co/products/5f0d9ba5385e3156009bd698?productName=D18%20Smart%20Watch",
+    name : "D18 Smart Watch",
+    price : "190 EGP",
+  },
+  "0006" : {
+    link : "https://beta.picksell.io/product/%d9%85%d8%ac-%d8%ad%d8%b1%d8%a7%d8%b1%d9%8a-500-%d9%85%d9%84/",
+    name : "مج حراري 500 مل",
+    price : "140 EGP",
+  },
+  "0007" : {
+    link : "https://taager.co/products/6193be5284851b0014419930?name=%D9%83%D9%88%D8%AA%D8%B4%D9%8A%20%D8%A7%D9%8A%D8%B1%20%D8%AC%D9%88%D8%B1%D8%AF%D9%86%20%D9%87%D8%A7%D9%81%20%D8%A7%D8%A8%D9%8A%D8%B6%20*%20%D9%84%D8%A8%D9%86%D9%8A%20%2B%204%20%D9%82%D8%B7%D8%B9%20%D8%B4%D8%B1%D8%A8%D8%A7%D8%AA%20%D9%81%D9%88%D8%A7%D9%83%D9%87",
+    name : "كوتشي اير جوردن هاف ابيض * لبني + 4 قطع شربات فواكه",
+    price : "240 EGP",
+  },
+  "0008" : {
+    link : "https://taager.co/products/618949302f9fe300138b940a?name=%D9%83%D9%88%D8%AA%D8%B4%D9%8A%20%D8%A7%D9%8A%D8%B1%20%D8%AC%D9%88%D8%B1%D8%AF%D9%86%20%D9%87%D8%A7%D9%81%20%D8%A7%D8%A8%D9%8A%D8%B6",
+    name : "كوتشي اير جوردن هاف ابيض",
+    price : "170 EGP",
+  },
+  "0009" : {
+    link : "https://taager.co/products/61911a712f0a8c0013c881cd?name=%D9%87%D8%A7%D9%81%20%D8%A8%D9%88%D8%AA%20%D8%AC%D9%84%D8%AF",
+    name : "هاف بوت جلد",
+    price : "270 EGP",
+  },
+  "0010" : {
+    link : "https://taager.co/products/61813ec522722f001265fa76?productName=%D8%B3%D9%86%D9%8A%D9%83%D8%B1%D8%B2%20%D8%AC%D9%84%D8%AF%20%D9%84%D9%84%D9%85%D8%B4%D9%8A",
+    name : "سنيكرز جلد للمشي",
+    price : "85 EGP",
+  },
 }

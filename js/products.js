2212b2bc158e29e4662dab22f99e3ed5abc7500b
YousefEl-Black <99845367+YousefEl-Black@users.noreply.github.com>
2022-02-19 22:43:09 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,17 +2,21 @@ const myProducts = {
   "0001" : {
     link : "https://beta.picksell.io/product/%d8%a8%d9%88%d8%aa-%d8%ac%d9%84%d8%af-%d8%a8%d8%b1%d8%a8%d8%a7%d8%b7-%d8%a8%d9%8a%d8%ac-%d9%85%d9%82%d8%a7%d8%b338/",
     name : "بوت جلد برباط بيج",
+    price : "450 EGP",
   },
   "0002" : {
     link : "https://beta.picksell.io/product/%d8%af%d9%81%d8%a7%d9%8a%d8%a9-%d9%83%d9%87%d8%b1%d8%a8%d8%a7%d8%a6%d9%8a%d8%a9-%d8%a8%d9%82%d9%88%d8%a9-2000-%d9%88%d8%a7%d8%b7/",
     name : "دفاية كهربائية بقوة 2000 واط",
+    price : "450 EGP",
   },
   "0003" : {
     link : "https://beta.picksell.io/product/%d9%85%d9%83%d9%88%d8%a9-%d8%a7%d9%84%d8%b4%d8%b9%d8%b1-%d8%a7%d9%86%d9%81%d8%b1%d8%a7-%d8%b1%d8%af/",
     name : "مكوة الشعر انفرا رد",
+    price : "220 EGP",
   },
   "0004" : {
     link : "https://taager.com/products/61ed9358ccb1560014a91cb0?productName=%D9%81%D8%B1%D8%B4%D8%A7%D8%A9%20%D8%A7%D9%84%D8%B4%D8%B9%D8%B1%20%20SilverCrest%20Creamic",
     name : "فرشاة الشعر SilverCrest Creamic",
+    price : "240 EGP",
   }
 }
